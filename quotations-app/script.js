644cc9939dbb2b4bb5ff0b8aaf391165adf8493a
Hellyes31 <elies.auguet@gmail.com>
2025-07-04 11:01:16 +0200

    Etape 6

--- quotations-app/script.js
+++ quotations-app/script.js
@@ -2,7 +2,7 @@ let text = document.getElementById("inputQuotes");
 let author = document.getElementById("inputAuthors");
 let selecteurs = document.getElementById("Selecteurs");
 let quoteList = document.getElementById("quote-list")
-
+let quoteCount = 0
 
 
 selecteurs.addEventListener("submit", function(e){
@@ -14,6 +14,8 @@ function addQuote(quote, author){
     let paragraphText = document.createElement("p");
     let paragraphAuthor = document.createElement("p");
     let divQuote = document.createElement("div");
+    let quoteCountHtml = document.getElementById("count") 
+
     paragraphText.setAttribute("class", "text");
     paragraphAuthor.setAttribute("class", "author");
     divQuote.setAttribute("class", "quote");
@@ -24,5 +26,8 @@ function addQuote(quote, author){
     divQuote.appendChild(paragraphText);  
     divQuote.appendChild(paragraphAuthor);
     quoteList.appendChild(divQuote);
+    quoteCount +=1;
+    quoteCountHtml.innerHTML = quoteCount + " citations"
 }
+
 
